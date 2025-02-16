71d6006a568ab0cbb77b9abd6d93b4d6b9113a0b
Wan Lopes <wan-lopes1@hotmail.com>
2025-02-16 20:56:16 -0300

    Alteração de serverjs

--- src/server.js
+++ src/server.js
@@ -18,6 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log ("Boas vindas a API do AllBooks")
-  console.log("API disponível através da url http://localhost:8000")
+  console.log("Acesse essa API por essa url http://localhost:8000")
 })
